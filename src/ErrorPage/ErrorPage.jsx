2094f111d713a2b087074094ba544f6397a5f579
Shovon96 <fakhruddinshovon@gmail.com>
2023-09-25 02:22:49 +0600

    error page added

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+const ErrorPage = () => {
+    return (
+        <div className="h-[90vh] flex justify-center items-center">
+            <div className="text-4xl font-bold text-center">
+                <h1>Opps!!!</h1>
+                <h1>No Data Found</h1>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../MainLayout/Home";
 import HomePage from "../componets/HomePage/HomePage";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 const myCreatedRouter = createBrowserRouter([
     {
         path: "/",
         element: <Home></Home>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
